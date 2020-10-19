e6abaedfb34ca3763a5c5d461533ca58fcca850a
karpach2000 <karpach2000@yandex.ru>
2020-10-19 18:19:02 +0300

    Добавил степень вложенности 2

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -27,10 +27,73 @@ public class Main
 
     public static void main(String[] args)
     {
+        nestingDegree2();
+
+
+    }
+    private static void nestingDegree2()
+    {
+        ArrayList<Tuple2<Long, Long>> calls =new  ArrayList<Tuple2<Long, Long>>();
+        calls.add(new Tuple2(1l, 2l));
+        calls.add(new Tuple2(1l, 3l));
+        calls.add(new Tuple2(2l, 5l));
+        calls.add(new Tuple2(2l, 6l));
+        calls.add(new Tuple2(3l, 7l));
 
 
 
+        //вводим данные
+        SparkConf sparkConf = new SparkConf().setAppName("Spark hi").setMaster("local[*]");
+        JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+        JavaRDD<Tuple2<Long, Long>> callRdd=sparkContext.parallelize(calls);
 
+
+        //преобразуем данные в формат содержащий выходные/промежуточные данные
+        JavaRDD<Tuple3<Long, Long, HashMap<Long, HashSet<Long>>>> connections = callRdd.distinct().//сжимаем входные данные
+                map(call ->
+        {
+            Tuple3<Long, Long, HashMap<Long, HashSet<Long>>> tuple =
+                    new Tuple3(call._1, call._2, new HashMap<Long, HashSet<Long>>());
+            return tuple;
+        });
+
+        //распечатываем данные (для удобства просмотра вывода)
+        System.out.println("connections:");
+        connections.foreach(item->System.out.println("A: "+ item._1()+" B:"+item._2() + " Connections: "+ item._3()));
+
+        //производим окончательные вычисления.
+        Tuple3<Long, Long, HashMap<Long, HashSet<Long>>> nestingDegree1 = connections.reduce((a, b) -> {
+            Tuple3<Long, Long, HashMap<Long, HashSet<Long>>> tuple = new Tuple3<>(0L, 0L,a._3());
+            b._3().forEach((k,v)-> {
+                if(tuple._3().containsKey(k))
+                {
+                    tuple._3().get(k).addAll(v);
+                }
+                else
+                {
+                    tuple._3().put(k,v);
+                }
+            });
+            tuple._3().putIfAbsent(a._1(),new  HashSet<Long>());
+            tuple._3().putIfAbsent(b._1(), new  HashSet<Long>());
+
+            tuple._3().get(a._1()).add(a._2());
+            if(tuple._3().containsKey(a._2()))
+                tuple._3().get(a._1()).addAll(tuple._3().get(a._2()));
+
+            tuple._3().get(b._1()).add(b._2());
+            if(tuple._3().containsKey(b._2()))
+                tuple._3().get(b._1()).addAll(tuple._3().get(b._2()));
+
+            return tuple;
+        });
+        nestingDegree1._3().remove(0);
+        System.out.println("Nesting degree 1: " + nestingDegree1._3());
+        sparkContext.close();
+    }
+
+    private static void nestingDegree1()
+    {
         ArrayList<Tuple2<Long, Long>> calls =new  ArrayList<Tuple2<Long, Long>>();
         calls.add(new Tuple2(1l, 2l));
         calls.add(new Tuple2(3l, 5l));
@@ -62,8 +125,8 @@ public class Main
         connections.foreach(item->System.out.println("A: "+ item._1()+" B:"+item._2() + " Connections: "+ item._3()));
 
         //производим окончательные вычисления.
-        Tuple3<Long, Long, HashMap<Long, HashSet<Long>>> ans = connections.reduce((a, b) -> {
-            Tuple3<Long, Long, HashMap<Long, HashSet<Long>>> tuple = new Tuple3<Long, Long, HashMap<Long, HashSet<Long>>>(0L, 0L,a._3());
+        Tuple3<Long, Long, HashMap<Long, HashSet<Long>>> nestingDegree1 = connections.reduce((a, b) -> {
+            Tuple3<Long, Long, HashMap<Long, HashSet<Long>>> tuple = new Tuple3<>(0L, 0L,a._3());
             b._3().forEach((k,v)-> {
                 if(tuple._3().containsKey(k))
                 {
@@ -74,10 +137,6 @@ public class Main
                     tuple._3().put(k,v);
                 }
             });
-
-
-
-
             tuple._3().putIfAbsent(a._1(), new  HashSet<Long>());
             tuple._3().putIfAbsent(b._1(), new  HashSet<Long>());
 
@@ -86,11 +145,9 @@ public class Main
 
             return tuple;
         });
-        System.out.println("Anser: " + ans);
+        nestingDegree1._3().remove(0);
+        System.out.println("Nesting degree 1: " + nestingDegree1._3());
         sparkContext.close();
-
-
-
     }
 
 
